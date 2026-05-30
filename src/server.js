75c85a6d954a9dd3857452f586f627892133760b
Иванушка <ccclownery@gmail.com>
2026-05-30 19:34:08 +0300

    Fix tasks routes

--- src/server.js
+++ src/server.js
@@ -4,35 +4,32 @@ const express = require('express');
 const cors = require('cors');
 
 const projectRoutes = require('./routes/projects.routes');
+const taskRoutes = require('./routes/tasks.routes');
 
 const app = express();
 
-app.use(cors({
-  origin: "https://kanban-frontend-two-tau.vercel.app"
-}));
+app.use(
+  cors({
+    origin: [
+      'http://localhost:5173',
+      'https://kanban-frontend-two-tau.vercel.app',
+    ],
+  })
+);
 
 app.use(express.json());
 
 app.use('/api/projects', projectRoutes);
+app.use('/api/tasks', taskRoutes);
 
-const tasks = [
-  {
-    id: 1,
-    title: 'Создать frontend',
-    description: 'Разработать интерфейс проекта',
-    status: 'backlog',
-    assignee: 'Иван',
-    due_date: '2026-05-10',
-    project_id: 1,
-  },
-];
-
-app.get('/api/tasks', (req, res) => {
-  res.json(tasks);
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Kanban API работает',
+  });
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log('Server started on port ' + PORT);
+  console.log(`Server started on port ${PORT}`);
 });
